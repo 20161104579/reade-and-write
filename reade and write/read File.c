de322700204ca9ab4fc259b8a626f5051e6d9d72
dreamer3c <2978425474@qq.com>
2017-06-12 09:59:16 +0800

    dreamer3c
    File

---
+++ reade and write/read File.c
@@ -0,0 +1,14 @@
+//
+//  read File.c
+//  reade and write
+//
+//  Created by 20161104579 on 2017/6/12.
+//  Copyright © 2017年 20161104579. All rights reserved.
+//
+
+#include "read File.h"
+int main()
+{
+    FILE *fp;
+    
+}
---
+++ reade and write/read File.h
@@ -0,0 +1,14 @@
+//
+//  read File.h
+//  reade and write
+//
+//  Created by 20161104579 on 2017/6/12.
+//  Copyright © 2017年 20161104579. All rights reserved.
+//
+
+#ifndef read_File_h
+#define read_File_h
+
+#include <stdio.h>
+
+#endif /* read_File_h */
