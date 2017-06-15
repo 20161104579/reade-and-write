0301ba95249b8b684f96281e6445777cc012d603
dreamer3c <2978425474@qq.com>
2017-06-15 10:55:41 +0800

    dreamer3c
    new

--- reade and write/read File.c
+++ reade and write/read File.c
@@ -9,6 +9,22 @@
 #include "read File.h"
 int main()
 {
+    char str[80];
+    int n;
+    float weight;
     FILE *fp;
-    
+    fp=fopen("/Users/a20161104579/Library/Mobile Documents/com~apple~TextEdit/Documents/format.rtf","r");
+    if(fp==NULL)
+    {
+        printf("error!");
+        return 1;
+    }
+    else{
+        while(fscanf(fp,"%s %d,%f",str,&n,&weight)!=EOF)
+        {
+            printf("%s %d %.2ff\n",str,n,weight);
+        }
+        fclose(fp);
+    }
+    return 0;
 }
